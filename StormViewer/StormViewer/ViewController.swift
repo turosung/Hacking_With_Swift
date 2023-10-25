0da44fc43140c40e0c7c6d4e58a9c6810693ea61
Nuhu Sulemana <turosung@gmail.com>
2023-10-25 22:31:50 +0100

    created navigation view and table view to display names of images

--- StormViewer/StormViewer/ViewController.swift
+++ StormViewer/StormViewer/ViewController.swift
@@ -7,13 +7,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    var pictures: [String] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let fm = FileManager.default
+        guard let path = Bundle.main.resourcePath else {
+            return
+        }
+        do {
+            let items = try fm.contentsOfDirectory(atPath: path)
+            for item in items {
+                if item.hasPrefix("nssl") {
+                    // Append picture here
+                    pictures.append(item)
+                }
+            }
+        } catch {
+            print("Images not found")
+        }
+        print(pictures)
     }
-
-
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
 }
 
